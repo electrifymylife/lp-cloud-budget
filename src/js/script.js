86c312a70c57994cabac9b08167bdcff6bda9055
Galina <vikhrovagp@gmail.com>
2023-01-31 02:37:30 +0300

    version1: add adaptive styles, replace fonts

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,14 @@
-// window.addEventListener('load', function() {
-//   const smoothLinks = document.querySelectorAll('a[href^="#"]');
-//   for (let smoothLink of smoothLinks) {
-//     smoothLink.addEventListener('click', function (e) {
-//       e.preventDefault();
-//       const id = smoothLink.getAttribute('href');
-//
-//       document.querySelector(id).scrollIntoView({
-//         behavior: 'smooth',
-//         block: 'start'
-//       });
-//     });
-//   };
-// });
+window.addEventListener('load', function() {
+  const smoothLinks = document.querySelectorAll('a[href^="#"]');
+  for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+      e.preventDefault();
+      const id = smoothLink.getAttribute('href');
+
+      document.querySelector(id).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  };
+});
